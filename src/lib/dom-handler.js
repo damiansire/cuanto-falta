453390a92aba2ce2e9d7c0b03770438e19cecaa7
Damian Sire <damiansire@hotmail.com>
2023-11-09 08:53:41 -0300

    feat: a leading 0 is displayed for numbers with a single digit

--- src/lib/dom-handler.js
+++ src/lib/dom-handler.js
@@ -1,7 +1,7 @@
 function updateCoutdownInScreen({ hours, minutes, seconds }) {
-  document.getElementById("countdown-hours").innerHTML = hours;
-  document.getElementById("countdown-minutes").innerHTML = minutes;
-  document.getElementById("countdown-seconds").innerHTML = seconds;
+  document.getElementById("countdown-hours").innerHTML = hours.toString().padStart(2, '0');
+  document.getElementById("countdown-minutes").innerHTML = minutes.toString().padStart(2, '0');
+  document.getElementById("countdown-seconds").innerHTML = seconds.toString().padStart(2, '0');
 }
 
 export { updateCoutdownInScreen };
